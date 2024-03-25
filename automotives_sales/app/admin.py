eb862f64f8dd47240179718f52ed973286dbf569
+ <+>
2024-03-25 10:38:26 +0300

    Login and sign up working as expected

--- automotives_sales/app/admin.py
+++ automotives_sales/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import UserProfile, Car, Invoice, Customer, Sale
 # Register your models here.
+
+# admin.site.register(UserProfile)
+admin.site.register(Car)
+admin.site.register(Invoice)
+admin.site.register(Customer)
+admin.site.register(Sale)
--- automotives_sales/app/forms.py
+++ automotives_sales/app/forms.py
@@ -1,22 +1,32 @@
-# forms.py
 from django import forms
-from .models import Car,Invoice
-# from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Car, Invoice
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    username = forms.CharField(max_length=30, required=True)
+    password1 =forms.CharField()
+    password2 =forms.CharField()
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email is already in use.')
+        return email
 
 class CarUpdateForm(forms.ModelForm):
     class Meta:
         model = Car
-        fields = '__all__'  # You can customize this to include specific fields you want to update
+        fields = '__all__'
+
 class InvoiceForm(forms.ModelForm):
     class Meta:
         model = Invoice
-        fields = '__all__'  # You can also specify specific fields if neede
+        fields = '__all__'
